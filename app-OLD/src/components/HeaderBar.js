9e3a7278ce036f43c3265823972285706726cd1c
Tony Kieling <tony.kieling@gmail.com>
2021-03-20 19:14:25 -0700

    settings for deploying done

--- app-OLD/src/components/HeaderBar.js
+++
@@ -1,9 +0,0 @@
-export default function HeaderBar() {
-
-  const message = "Free Shipping On Orders Over $49"
-  return(
-    <div className = "header-bar">
-      { message }
-    </div>
-  )
-}
--- app-OLD/src/components/SearchResult.js
+++
@@ -1,9 +0,0 @@
-export default function SearchResult(){
-  return (
-    <div className = "app-body" style = {{color: "blue", fontWeight: "bolder"}}>
-      <h2>
-        Search Result Page
-      </h2>
-    </div>
-  );
-}
